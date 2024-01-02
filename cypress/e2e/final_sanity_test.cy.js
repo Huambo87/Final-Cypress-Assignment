81aca6d42786524a8d06353ad5f6dcfea43762ad
Huambo87 <maximgladkov@hotmail.com>
2024-01-02 17:53:26 -0500

    Changes to be committed: 	modified:   cypress/e2e/final_sanity_test.cy.js 	modified:   cypress/support/final_selectors.js

--- cypress/e2e/final_sanity_test.cy.js
+++ cypress/e2e/final_sanity_test.cy.js
@@ -1,6 +1,6 @@
 ///<reference types = 'Cypress'/>
 
-// import selectors from '../support/final_selectors.js'
+import selectors from '../support/final_selectors.js'
 
 describe('Home Page Test', () => {
 
@@ -15,53 +15,103 @@ describe('Home Page Test', () => {
     // cy.get('body').should('be.visible')
   })
 
+  // it('home_tab_test', () => {
+  //   cy.get('[id="menu-item-31"]')
+  //   cy.get('h1').should('not.be.empty')
+  // })
+  
+  // it('about_tab_test', () => {
+  //   cy.get('[id="menu-item-36"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('products_tab_test', () => {
+  //   cy.get('[id="menu-item-32"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('services_tab_test', () => {
+  //   cy.get('[id="menu-item-34"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('tools_tab_test', () => {
+  //   cy.get('[id="menu-item-35"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('promotions_tab_test', () => {
+  //   cy.get('[id="menu-item-673"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('careers_tab_test', () => {
+  //   cy.get('[id="menu-item-1589"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('blog_tab_test', () => {
+  //   cy.get('[id="menu-item-1725"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('faq_tab_test', () => {
+  //   cy.get('[id="menu-item-1727"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
+  // it('contacts_tab_test', () => {
+  //   cy.get('[id="menu-item-1726"]').click()
+  //   cy.get('h1').should('not.be.empty')
+  // })
+
   it('home_tab_test', () => {
-    cy.get('[id="menu-item-31"]')
+    cy.get(selectors.home_tab)
     cy.get('h1').should('not.be.empty')
   })
   
   it('about_tab_test', () => {
-    cy.get('[id="menu-item-36"]').click()
+    cy.get(selectors.about_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('products_tab_test', () => {
-    cy.get('[id="menu-item-32"]').click()
+    cy.get(selectors.products_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('services_tab_test', () => {
-    cy.get('[id="menu-item-34"]').click()
+    cy.get(selectors.services_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('tools_tab_test', () => {
-    cy.get('[id="menu-item-35"]').click()
+    cy.get(selectors.tools_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('promotions_tab_test', () => {
-    cy.get('[id="menu-item-673"]').click()
+    cy.get(selectors.promotions_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('careers_tab_test', () => {
-    cy.get('[id="menu-item-1589"]').click()
+    cy.get(selectors.careers_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('blog_tab_test', () => {
-    cy.get('[id="menu-item-1725"]').click()
+    cy.get(selectors.blog_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('faq_tab_test', () => {
-    cy.get('[id="menu-item-1727"]').click()
+    cy.get(selectors.faq_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 
   it('contacts_tab_test', () => {
-    cy.get('[id="menu-item-1726"]').click()
+    cy.get(selectors.conatacts_tab).click()
     cy.get('h1').should('not.be.empty')
   })
 })
