40aedde588da016be448437b9903daae921f9f7e
Huambo87 <maximgladkov@hotmail.com>
2024-01-02 19:51:54 -0500

    Changes to be committed: 	modified:   cypress/e2e/final_sanity_test.cy.js

--- cypress/e2e/final_sanity_test.cy.js
+++ cypress/e2e/final_sanity_test.cy.js
@@ -22,46 +22,46 @@ describe('Home Page Test', () => {
   
   it('about_tab_test', () => {
     cy.get(selectors.about_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('About Consumers Energy Management Inc.')
   })
 
   it('products_tab_test', () => {
     cy.get(selectors.products_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Products')
   })
 
   it('services_tab_test', () => {
     cy.get(selectors.services_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Services')
   })
 
   it('tools_tab_test', () => {
     cy.get(selectors.tools_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Tools')
   })
 
   it('promotions_tab_test', () => {
     cy.get(selectors.promotions_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Promotions')
   })
 
   it('careers_tab_test', () => {
     cy.get(selectors.careers_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Careers')
   })
 
   it('blog_tab_test', () => {
     cy.get(selectors.blog_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Blog')
   })
 
   it('faq_tab_test', () => {
     cy.get(selectors.faq_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Frequently')
   })
 
   it('contacts_tab_test', () => {
     cy.get(selectors.conatacts_tab).click()
-    cy.get('h1').should('not.be.empty')
+    cy.get('h1').should('not.be.empty').contains('Contact')
   })
 })
