283f94996b2ac3d2d0519ca390b5e731d95eeb4e
Huambo87 <maximgladkov@hotmail.com>
2024-04-14 21:27:16 -0400

    Changes to be committed: 	modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     'baseUrl':'https://consumersenergymanagement.ca/',
     setupNodeEvents(on, config) {
